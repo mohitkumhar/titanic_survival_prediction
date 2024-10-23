5c1fd6f24440ec8e7b8ce199792b7da0d1b2ad7d
Mohit Kumhar <mohitmolela@gmail.com>
2024-10-23 22:11:36 +0530

    fix bugs

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def predict():
     
     prediction = model.predict(input_data)
     
-    return render_template('index.html', prediction_text=f'{ 'Survive' if prediction[0] == 1 else 'Not - Survive' }')
+    return render_template('index.html', prediction_text='Survive' if prediction[0] == 1 else 'Not - Survive')
 
 if __name__ == '__main__':
     app.run(debug=True)
